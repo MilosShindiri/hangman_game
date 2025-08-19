04d2230e1f8135e70f78f268b8089db1d6d209fa
Milos_Petrovic <milos.petrovic@shindiristudio.com>
2025-08-19 13:07:54 +0200

    refactoring commponents and adding them up in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,80 @@
-import React from "react";
-
-/**
- * Project planning:
- *
- * Questions to ask yourself before writing any code:
- *
- * - What are the main containers of elements I need
- *   in this app?
- * - What values will need to be saved in state vs.
- *   what values can be derived from the state?
- * - How will the user interact with the app? What
- *   events do I need to handle?
- */
+import { useState, useRef, useEffect } from "react";
+import { clsx } from "clsx";
+import { languages } from "/utils/languages.js";
 
 export default function Hangman() {
-  return <main>Game goes here</main>;
+  const [word, setWord] = useState("react");
+  const [guess, setGuess] = useState([]);
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  function addGuess(letter) {
+    setGuess((prevLetters) =>
+      prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter]
+    );
+  }
+
+  const langElements = languages.map((lang) => (
+    <span
+      className="language"
+      style={{ backgroundColor: lang.backgroundColor, color: lang.color }}
+      key={lang.name}
+    >
+      {lang.name}
+    </span>
+  ));
+
+  let letters = word.split("").map((letter, index) => (
+    <span className="letter" key={index}>
+      {letter.toUpperCase()}
+    </span>
+  ));
+
+  let characters = alphabet.split("").map((character, index) => {
+    const isGuessed = guess.includes(character);
+    const isCorrect = isGuessed && word.includes(character);
+    const isWrong = isGuessed && !word.includes(character);
+    const className = clsx(
+      "character", // default class
+      {
+        correct: isCorrect,
+        wrong: isWrong,
+      }
+    );
+
+    return (
+      <button
+        className={className}
+        key={index}
+        onClick={() => addGuess(character)}
+      >
+        {character.toUpperCase()}
+      </button>
+    );
+  });
+
+  return (
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      <section className="status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+      <section>
+        <div className="languages">{langElements}</div>
+      </section>
+      <section>
+        <div className="displayWord">{letters}</div>
+      </section>
+      <section>
+        <div className="keyboard">{characters}</div>
+      </section>
+    </main>
+  );
 }
