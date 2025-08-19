72c7f1d40e899cd4b4104fb4d0eae9bcb20e46b4
Milos_Petrovic <milos.petrovic@shindiristudio.com>
2025-08-19 16:19:56 +0200

    adding random words

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import { useState, useRef, useEffect } from "react";
 import { clsx } from "clsx";
 import { languages } from "/utils/languages.js";
+import { words } from "/utils/words.js";
+import Confetti from "react-confetti";
+
+function getRandomWord() {
+  const randomIndex = Math.floor(Math.random() * words.length);
+  return words[randomIndex];
+}
+
 //letters are for displaying the word
 //chracters are for keyboard characters
 export default function Hangman() {
-  const [word, setWord] = useState("react");
+  // State values
+  const [word, setWord] = useState(() => getRandomWord());
   const [guess, setGuess] = useState([]);
 
+  // Derived values
+
+  const wrongGuess = guess.filter(
+    (character) => !word.includes(character)
+  ).length;
+
+  const isGameWon = word.split("").every((letter) => guess.includes(letter));
+  const isGameLost = wrongGuess >= 8;
+
+  const isGameOver = isGameWon || isGameLost;
+
+  // Static values
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
   // user guesses - clicked on character
@@ -17,15 +38,18 @@ export default function Hangman() {
   }
 
   // displaying languages(html,css,js,raect,ts,node,py,ruby,assembly)
-  const langElements = languages.map((lang) => (
-    <span
-      className="language"
-      style={{ backgroundColor: lang.backgroundColor, color: lang.color }}
-      key={lang.name}
-    >
-      {lang.name}
-    </span>
-  ));
+  const langElements = languages.map((lang, index) => {
+    const isLanguageLost = index < wrongGuess;
+    return (
+      <span
+        className={`language ${isLanguageLost ? "lost" : ""}`}
+        style={{ backgroundColor: lang.backgroundColor, color: lang.color }}
+        key={lang.name}
+      >
+        {lang.name}
+      </span>
+    );
+  });
   // this is for displaying the word
   let letters = word.split("").map((letter, index) => (
     <span className="letter" key={index}>
@@ -50,11 +74,17 @@ export default function Hangman() {
         className={className}
         key={index}
         onClick={() => addGuess(character)}
+        disabled={isGameOver}
       >
         {character.toUpperCase()}
       </button>
     );
   });
+
+  function Reset() {
+    setGuess([]);
+    setWord(getRandomWord());
+  }
 
   return (
     <main>
@@ -65,10 +95,19 @@ export default function Hangman() {
           from Assembly!
         </p>
       </header>
-      <section className="status">
-        <h2>You win!</h2>
-        <p>Well done! 🎉</p>
-      </section>
+      {isGameWon &&
+        (<Confetti />)(
+          <section className="statusWon">
+            <h2>You win!</h2>
+            <p>Well done! 🎉</p>
+          </section>
+        )}
+      {isGameLost && (
+        <section className="statusLost">
+          <h2>Game over!</h2>
+          <p>You lose! Better start learning Assembly 😭</p>
+        </section>
+      )}
       <section>
         <div className="languages">{langElements}</div>
       </section>
@@ -78,6 +117,11 @@ export default function Hangman() {
       <section>
         <div className="keyboard">{characters}</div>
       </section>
+      {isGameOver && (
+        <button className="new-game" onClick={Reset}>
+          New Game
+        </button>
+      )}
     </main>
   );
 }
